96ce9874696a8bcac83b22e8e5c4eb399747cdfb
Moises Miguel <moimigueldev@gmail.com>
2021-04-28 17:53:20 -0500

    created double sided slider

--- main/script.js
+++ main/script.js
@@ -1,23 +1,61 @@
-const slider = document.getElementById('slider');
-const selector = document.getElementById('selector');
-const selectValue = document.getElementById('selectValue');
-const progressBar = document.getElementById('progress-bar');
-const selectHover = document.querySelector('.select-hover');
+function getVals() {
+  // Get slider values
+  var parent = this.parentNode;
+  var slides = document.querySelectorAll('input');
+  // var slides = parent.getElementsByTagName('input');
 
-slider.oninput = function () {
-  selector.style.left = this.value + '%';
-  //   selectValue.innerHTML = this.value;
-  progressBar.style.width = this.value + '%';
+  var slide1 = parseFloat(slides[0].value);
+  var slide2 = parseFloat(slides[1].value);
+  // Neither slider will clip the other, so make sure we determine which is larger
+  if (slide1 > slide2) {
+    var tmp = slide2;
+    slide2 = slide1;
+    slide1 = tmp;
+  }
+
+  // var displayElement = parent.getElementsByClassName('rangeValues')[0];
+  // var displayElement = document.getElementsByClassName('rangeValues')[0];
+  // displayElement.innerHTML = slide1 + ' - ' + slide2;
+}
+
+window.onload = function () {
+  // Initialize Sliders
+  var sliderSections = document.getElementsByClassName('range-slider');
+  for (var x = 0; x < sliderSections.length; x++) {
+    var sliders = sliderSections[x].getElementsByTagName('input');
+    for (var y = 0; y < sliders.length; y++) {
+      if (sliders[y].type === 'range') {
+        sliders[y].oninput = getVals;
+        // Manually trigger event first time to display values
+        sliders[y].oninput();
+      }
+    }
+  }
 };
 
-selector.addEventListener('mouseover', function () {
-  console.log('hello', selectHover);
-  selectHover.classList.add('show');
+// ===================
+// ===================
+
+var slider1 = document.getElementById('slider-1');
+var slider2 = document.getElementById('slider-2');
+
+var progressBar = document.getElementById('progress-bar');
+var progressBar2 = document.getElementById('progress-bar-2');
+
+progressBar2.style.width = 100 - slider2.value;
+
+slider1.addEventListener('input', function () {
+  // progressBar.style.width = this.value + '%';
+
+  const elementPosition = this.getBoundingClientRect();
+
+  progressBar.style.width = this.value + '%';
 });
 
-selector.addEventListener('mouseleave', function () {
-  console.log('hello', selectHover);
-  selectHover.classList.remove('show');
-});
+slider2.addEventListener('input', function () {
+  // progressBar.style.width = this.value + '%';
 
-selector.onactive;
+  const elementPosition = this.getBoundingClientRect();
+
+  progressBar2.style.width = 100 - this.value + '%';
+});
